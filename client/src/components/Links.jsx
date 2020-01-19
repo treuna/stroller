9525ae6898482b75b77f18617810297125384df8
Teemu Reuna <teemu.reuna@unikie.com>
2020-01-19 19:33:18 +0200

    UI first draft?

---
+++ client/src/components/Links.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const Collapse = styled.div.attrs({
+  className: 'collapse navbar-collapse'
+})``
+
+const List = styled.div.attrs({
+  className: 'navbar-nav mr-auto'
+})``
+
+const Item = styled.div.attrs({
+  className: 'collapse navbar-collapse'
+})``
+
+class Links extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Link to="/" className="navbar-brand">
+          Stroller Troll!
+        </Link>
+        <Collapse>
+          <List>
+            <Item>
+              <Link to="/" className="nav-link">
+                Strollers
+              </Link>
+            </Item>
+            <Item>
+              <Link to="/create" className="nav-link">
+                Create Stroller
+              </Link>
+            </Item>
+          </List>
+        </Collapse>
+      </React.Fragment>
+    )
+  }
+}
+
+export default Links
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+import Logo from './Logo'
+import Links from './Links'
+
+const Container = styled.div.attrs({
+  className = 'container',
+})``
+
+const Nav = styled.div.attrs({
+  className: 'navbar navbar-expand-lg navbar-darg bg-dark'
+})`
+  margin-bottom: 20 px;
+`
+
+class NavBar extends Component {
+  render(){
+    return (
+      <Container>
+        <Nav>
+          <Logo />
+          <Links />
+        </Nav>
+      </Container>
+      )
+  }
+}
+
+export default NavBar
