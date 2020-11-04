f4b844b7b861cbc3e70d39b9df482f4a4ea8964b
Teemu Reuna <teemu.reuna@unikie.com>
2020-11-04 08:59:13 +0200

    Fixed broken link

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -10,7 +10,7 @@ const Wrapper = styled.a.attrs({
 class Logo extends Component {
   render() {
     return (
-      <Wrapper href="https://sambarros.com">
+      <Wrapper href="https://reactjs.org">
           <img src={logo} width="50" height="50" alt="sambarros.com" />
       </Wrapper>
     )
